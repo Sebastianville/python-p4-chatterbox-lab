24af13e73d57964bf2d03cb33ccdb64d1b47730b
Sebastian Villegas <Sebastian.villegas2623@gmail.com>
2024-09-21 14:50:10 -0400

    completed

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
 
+
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,13 +15,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = [message.to_dict() for message in Message.query.all()]
 
-@app.route('/messages/<int:id>')
+        return make_response(messages, 200)
+
+    elif request.method == "POST":
+        new_message = Message(
+            body=request.json["body"],
+            username=request.json["username"],
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+        new_message_dict = new_message.to_dict()
+
+        return make_response(new_message_dict, 201)      
+
+@app.route('/messages/<int:id>', methods=["GET", "PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    if request.method == "GET":
+        message = Message.query.filter_by(id=id).first()
+        message_dict = message.to_dict()
+        return make_response(message_dict, 200)
+
+    elif request.method == "PATCH":
+        message = Message.query.filter_by(id=id).first()
+        message.body = request.json["body"]
+        db.session.add(message)
+        db.session.commit()
+        message_dict = message.to_dict()
+        return make_response(message_dict, 200)
+
+    elif request.method == "DELETE":
+        message = Message.query.filter_by(id=id).first()
+        db.session.delete(message)
+        db.session.commit()
+
+        return make_response("", 204)
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -10,5 +10,11 @@ db = SQLAlchemy(metadata=metadata)
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
-
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"<{self.id}. {self.username}: {self.body} | {self.created_at}>"
